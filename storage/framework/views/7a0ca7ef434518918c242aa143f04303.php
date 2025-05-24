8d247b1add9e32b45830121dd4fb3436348c074d
sa st <sabstha98@gmail.com>
2025-05-25 08:05:11 +0900

    aa

--- storage/framework/views/7a0ca7ef434518918c242aa143f04303.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<div class="w-full px-4 py-6">
-    <div id="pos-app" class="max-w-screen-xl mx-auto"></div>
-</div>
-<?php $__env->stopSection(); ?>
-
-<?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
-
-<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\sabst\momo_shop\resources\views/pos.blade.php ENDPATH**/ ?>
