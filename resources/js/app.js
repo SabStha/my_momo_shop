0bf90010133731ab0a7d6e64ead23e345a8b20f9
Sabin Shrestha <sabstha98@gmail.com>
2025-07-05 04:10:20 +0900

    aa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './clock-system.js';
 import './wallet.js';
+import '../scss/app.scss' if you're using SCSS
+
 
 // Initialize any global JavaScript functionality
 document.addEventListener('DOMContentLoaded', function() {
