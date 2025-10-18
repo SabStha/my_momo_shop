b44b6a4482d75582a48abaa60a51f8d14020a6c6
Sabin Shrestha <sabstha98@gmail.com>
2025-10-19 01:11:04 +0900

    aa

--- app/Http/Controllers/SalesAnalyticsController.php
+++ app/Http/Controllers/SalesAnalyticsController.php
@@ -44,7 +44,20 @@ class SalesAnalyticsController extends Controller
         $endDate = $request->input('end_date', now()->format('Y-m-d'));
         $branchId = $request->input('branch', session('selected_branch_id'));
 
+        \Log::info('📊 Sales Analytics Page Accessed', [
+            'period' => $period,
+            'startDate' => $startDate,
+            'endDate' => $endDate,
+            'branchId' => $branchId
+        ]);
+
         $data = $this->salesAnalyticsService->getSalesOverview($period, $startDate, $endDate, $branchId);
+
+        \Log::info('📊 Sales Analytics Data Retrieved', [
+            'total_sales' => $data['summary']['total_sales'] ?? 0,
+            'total_orders' => $data['summary']['total_orders'] ?? 0,
+            'unique_customers' => $data['summary']['unique_customers'] ?? 0
+        ]);
 
         return view('admin.sales.overview', compact('data', 'period', 'startDate', 'endDate', 'branchId'));
     }
