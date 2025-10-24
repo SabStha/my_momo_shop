672ad57b1e09d8c4a46907dfbf5cc85790f89a77
Sabin Shrestha <sabstha98@gmail.com>
2025-10-25 02:41:12 +0900

    aa

--- app/Models/OfferAnalytics.php
+++ app/Models/OfferAnalytics.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class OfferAnalytics extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'offer_id',
+        'user_id',
+        'action',
+        'timestamp',
+        'device_info',
+        'session_data',
+        'notification_id',
+        'discount_value',
+    ];
+
+    protected $casts = [
+        'timestamp' => 'datetime',
+        'device_info' => 'array',
+        'session_data' => 'array',
+        'discount_value' => 'decimal:2',
+    ];
+
+    public function offer()
+    {
+        return $this->belongsTo(Offer::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function notification()
+    {
+        return $this->belongsTo(\Illuminate\Notifications\DatabaseNotification::class, 'notification_id');
+    }
 }
